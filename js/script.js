151edfb79f86895f92a0d391c3aed3c9937ab6fe
VincenzoZ24 <zitov287@gmail.com>
2022-10-21 20:17:10 +0200

    completata function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-let kmDaFare = document.getElementById("km_dafare");
+let kmDaFare = document.getElementById("km");
 let submit = document.getElementById("invia");
 let eta = document.getElementById("eta");
 
 submit.addEventListener("click", function biglietto() {
-    let kmDaFare = document.getElementById("km_dafare");
-    let eta = document.getElementById("eta");
-
+    let kmDaFare = document.getElementById("km");
+    let eta = document.getElementById("age");
 
     
-    let prezzoBiglietto = kmDaFare * "0.21";
-    document.getElementById("prezzo").innerHTML;
-    
-    if (eta < 18) {
+    let prezzoBiglietto = kmDaFare.value * "0.21";
+   
+    document.getElementById("prezzo").innerHTML = prezzoBiglietto + "€";
+    if (eta.value < 18) {
         let sconto1 = (prezzoBiglietto * 0.2);
         let prezzo1 = (prezzoBiglietto - sconto1);
         let prezzoScontato1 = prezzo1.toFixed(2);
-        
+       
+        document.getElementById("prezzo").innerHTML = prezzoScontato1 + "€";
     }
-    else if (eta > 65) {
+    else if (eta.value > 65) {
         let sconto2 = (prezzoBiglietto * 0.4);
         let prezzo2 = (prezzoBiglietto - sconto2);
         let prezzoScontato2 = prezzo2.toFixed(2);
-       
-
+        document.getElementById("prezzo").innerHTML = prezzoScontato2 + "€";
     }
 
    
-    
+   
 });
